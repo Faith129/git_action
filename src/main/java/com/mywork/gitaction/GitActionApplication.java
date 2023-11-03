364a08febcd59d70995d8366f2820de3c993afa0
Abu <adamuabu@ymail.com>
2023-11-03 09:24:14 +0100

    build and push

--- src/main/java/com/mywork/gitaction/GitActionApplication.java
+++ src/main/java/com/mywork/gitaction/GitActionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitActionApplication {
-//learning github action is interesting
 	public static void main(String[] args) {
 		SpringApplication.run(GitActionApplication.class, args);
 	}
