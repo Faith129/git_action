f630d97b5c134789d021a13eabf6025c2e8a5eec
Abu <adamuabu@ymail.com>
2023-11-03 10:06:05 +0100

    docker img

--- src/main/java/com/mywork/gitaction/GitActionApplication.java
+++ src/main/java/com/mywork/gitaction/GitActionApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitActionApplication {
 
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitActionApplication.class, args);
 	}
